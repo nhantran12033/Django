4a7ad6d94ff5c43627ae8a0b956aae7fc3ce9193
nhantran12033 <nhantran12033@gmail.com>
2023-10-12 10:22:13 +0700

    update

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,18 +78,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'pollsapp',
-        'USER': 'postgres',
-        'PASSWORD': 'uyennhi',
-        'HOST': 'localhost',
-        'PORT': '5432',
-
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "mydatabase",
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
